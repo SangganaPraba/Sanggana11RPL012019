d4af1ba48914aba26503f28f66281bbd2f08b757
SanganaPraba <sangganapraba@gmail.com>
2020-08-07 14:50:40 +0700

    add adapter rv

--- app/src/main/java/com/example/sanggana11rpl012019/MainActivity.java
+++ app/src/main/java/com/example/sanggana11rpl012019/MainActivity.java
@@ -2,15 +2,52 @@ package com.example.sanggana11rpl012019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtusername;
+    EditText txtpassword;
+    Button btnlogin;
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref = getSharedPreferences("login", MODE_PRIVATE);
+        txtusername = findViewById(R.id.et1);
+        txtpassword = findViewById(R.id.et2);
+        btnlogin = findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String et1key = txtusername.getText().toString();
+                String et2key = txtpassword.getText().toString();
+                if (txtusername.getText().toString().isEmpty() ||
+                        txtpassword.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Tidak boleh ada yang kosong", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    if (txtusername.getText().toString().equals("sanggana")
+                            && txtpassword.getText().toString().equals("0107")) {
+                        Toast.makeText(MainActivity.this, "Login success", Toast.LENGTH_SHORT).show();
+                        editor = pref.edit();
+                        editor.putString("userid", txtusername.getText().toString());
+                        editor.apply();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Login gagal", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
